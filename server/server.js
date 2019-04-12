fa112a23a614be1bfb7d83bd848ebb7f40f7af62
Lauren Irish <lirish2@gmail.com>
2019-04-12 15:36:30 -0600

    first push

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const authCtrl = require('./controllers/authCtrl')
+const workoutCtrl = require('./controllers/workoutCtrl')
+const movementsCtrl = require('./controllers/movementsCtrl')
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+const app = express();
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    app.listen(SERVER_PORT, () => {
+        console.log(`listening on ${SERVER_PORT}`)
+    })
+})
+
+app.use(express.json());
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Login endpoints
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+app.get('/auth/user-data', authCtrl.userData)
+app.get('/logout', authCtrl.logout)
+
+// Dashboard endpoints
+app.get('/api/user-workouts', workoutCtrl.readWorkouts)
+app.post('/api/user-workout', workoutCtrl.createWorkout)
+app.put('/api/user-workout/:id', workoutCtrl.updateWorkout)
+app.delete('/api/user-workout/:id', workoutCtrl.deleteWorkout)
+
+// Movement endpoints
+app.get('/api/movements', movementsCtrl.readMovements)
+app.post('/api/movement', movementsCtrl.createMovement)
+app.delete('/api/movement:id', movementsCtrl.deleteMovement)
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom'
+import Login from './components/Login/Login'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <Switch>
+          <Route exact path='/' component={Login}></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Switch>
+      </HashRouter>
     );
   }
 }
