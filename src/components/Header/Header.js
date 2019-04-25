22fb5c347e7af4960038985ae0441b930dec6623
Lauren Irish <lirish2@gmail.com>
2019-04-25 15:12:56 -0600

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,6 +29,7 @@ class Header extends Component {
        <div className="nav-links">
           <div onClick={() => this.props.history.push('/dashboard')}>Home</div>
           <div onClick={() => this.props.history.push('/createworkout')}>CW</div>
+          
             {/* <a href={`${process.env.REACT_APP_FRONT}/#/createworkout`}>Edit Workout</a>
             <a href={`${process.env.REACT_APP_FRONT}/#/addmove`}>Add Exercise</a>
             <a href={`${process.env.REACT_APP_BACK}/logout`}>Logout</a> */}
